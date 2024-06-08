685cbe0052a566b9fce8028f278f8f33f35d0ce8
shohruhbekk007 <shohruhbek007dev@gmail.com>
2024-06-08 11:45:47 +0500

    adminka o'zgartirish kiritilgdi

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
+    "unfold",  # before django.contrib.admin
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",  # optional, if django-guardian package is used
+    "unfold.contrib.simple_history", 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
 ]
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- main/admin.py
+++ main/admin.py
@@ -1,23 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from .models import *
 from .forms import ContractForm
+from unfold.admin import ModelAdmin
 
 
-admin.site.register(City)
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ("username", "is_active", "is_staff")
+    fields = ["username", "first_name", "last_name", "is_staff", "is_active", "email"]
+
+
+@admin.register(City)
+class CityAdmin(ModelAdmin):
+    list_display = ("name",)
+    fields = list_display
 
 @admin.register(Costumer)
-class CostumerAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'phone_number', 'mavzu')
+class CostumerAdmin(ModelAdmin):
+    list_display = ('first_name', 'last_name', 'father_name', 'phone_number', 'mavzu')
+    list_filter = ("mavzu", )
 
-    def changelist_view(self, request, extra_context=None):
-        response = super().changelist_view(request, extra_context)
-        if hasattr(response, 'context_data'):
-            cl = response.context_data['cl']
-            queryset = cl.result_list
-            for item in queryset:
-                item.row_class = 'colorize-row' if item.mavzu != 'sotib olmoqchi' else ''
-        return response
+    def get_row_color(self, obj):
+        return 'colorize-row' if obj.mavzu != 'sotib olmoqchi' else ''
+
+    get_row_color.short_description = 'Color'
+    get_row_color.admin_order_field = 'mavzu'
 
     class Media:
         css = {
@@ -25,9 +37,8 @@ class CostumerAdmin(admin.ModelAdmin):
         }
 
 
-
 @admin.register(Contract)
-class ContractAdmin(admin.ModelAdmin):
+class ContractAdmin(ModelAdmin):
     form = ContractForm
     list_display = ['city', 'full_name', 'room', 'monthTomoney']
     class Media:
